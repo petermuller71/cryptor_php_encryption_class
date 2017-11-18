464744dd07f6b8067821bb7b8801342111564e6d
Peter Muller <petermuller71@gmail.com>
2017-11-18 20:30:51 +0100

    Update cryptor.php

--- cryptor.php
+++ cryptor.php
@@ -6,9 +6,13 @@
  * PHP Encryption and decryption class with open_ssl
  * Works also with larger text (because text is split in smaller parts)
  *
- * Instruction:
+ * Instruction (no secret key provided):
  * encryption:  $encrypted_txt    = Cryptor::doEncrypt($plain_txt);
  * decryption:  $plain_txt        = Cryptor::doDecrypt($encrypted_txt);
+ *
+ * Instruction (with secret key):
+ * encryption:  $encrypted_txt    = Cryptor::doEncrypt($plain_txt, "secret key used for encryption");
+ * decryption:  $plain_txt        = Cryptor::doDecrypt($encrypted_txt, "secret key used for encryption");
  *
  * Change class properties (change secret keys, etc)!
  *
@@ -25,6 +29,7 @@ class Cryptor {
      *
      * @param      string       $plain_txt        Text, to be encrypted
      * @param      string       $encrypted_txt    Text, to be decrypted
+     * @param      string       $secretkey        Optional, override with (static private) property 
      * 
      * @property   int          $unique_nr        Amount of random characters put in front en behind encrypted string + used as salt
      * @property   int          $strspit_nr       Amount of characters to split source (<= 400!), open_ssl cannot encrypt large files
@@ -35,7 +40,7 @@ class Cryptor {
      * @return     string       Encrypted or decrypted text
      * 
      * @author     Peter Muller <petermuller71@gmail.com>
-     * @version    1.01
+     * @version    1.02
      *
      */
     
@@ -51,12 +56,15 @@ class Cryptor {
      * Encrypt text
      *
      * @param   string    $plain_txt   Text that will be encrypted
+     * @param   string    $secretkey   Optional, override with (static private) property
      * @return  string    Encrypted text
      *
      */
     
-    public static function doEncrypt($plain_txt) {
-       
+    public static function doEncrypt($plain_txt, $secretkey = null) {
+
+       if ($secretkey == null) { $secretkey = self::$secret_key; }    
+        
        // two unique values are created
        // used as salt and put in front + behind encrypted text
        
@@ -66,7 +74,7 @@ class Cryptor {
        // $plain_text should be split in smaller parts and encrypted seperatly
       
        $arr = str_split($plain_txt, self::$strspit_nr);
-       foreach ($arr as $v) { $encrypted_txt .= substr(self::doEncryptDecrypt('encrypt', $v), 0, -2)."_"; }
+       foreach ($arr as $v) { $encrypted_txt .= substr(self::doEncryptDecrypt('encrypt', $secretkey, $v), 0, -2)."_"; }
        
        $encrypted_txt = substr($encrypted_txt, 0, -1);
        
@@ -74,7 +82,7 @@ class Cryptor {
        
        $encrypted_txt = self::replace("go", $encrypted_txt);
        
-       return $encrypted_txt;
+       return $encrypted_txt; 
     }
  
     
@@ -84,12 +92,15 @@ class Cryptor {
      * Decrypt text
      *
      * @param   string    $encrypted_txt   Text that will be decrypted
+     * @param   string    $secretkey       Optional, override with (static private) property
      * @return  string    Decrypted text
      *
      */   
  
-   public static function doDecrypt($encrypted_txt) {
+   public static function doDecrypt($encrypted_txt, $secretkey = null) {
 
+       if ($secretkey == null) { $secretkey = self::$secret_key; } 
+       
        // smaller parts were glued together with underscore (_) and replaced by a letter      
        
        $encrypted_txt   = self::replace("back", $encrypted_txt);
@@ -97,7 +108,7 @@ class Cryptor {
        // $encrypted_txt should be split in smaller parts and decrypted seperatly
        
        $arr  = explode("_", $encrypted_txt);
-       foreach ($arr as $v) { $decrypted_txt .= self::doEncryptDecrypt('decrypt', $v); }
+       foreach ($arr as $v) { $decrypted_txt .= self::doEncryptDecrypt('decrypt', $secretkey, $v); }
              
        $decrypted_txt = substr($decrypted_txt, 10);
        
@@ -109,32 +120,31 @@ class Cryptor {
     * doEncryptDecrypt
     * Encrypt or decrypt text 
     *
-    * @param   string    $action   Encrypt or decrypt text
-    * @param   string    $source   Source that is encrypted or decrypted
+    * @param   string    $action     Encrypt or decrypt text
+    * @param   string    $secretkey  secretkey used for encryption/decryption
+    * @param   string    $source     Source that is encrypted or decrypted
     * @return  string
     *
     */
    
-    private static function doEncryptDecrypt($action, $source) {
-
+    private static function doEncryptDecrypt($action, $secretkey, $source) {
+       
         $output     = false;
-        $secret_key = self::$secret_key;
-        $secret_iv  = self::$secret_iv;
         
         // hash
-        $key = hash('sha512', $secret_key);
+        $secretkey  = hash('sha512', $secretkey);
         
         // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
-        $iv = substr(hash('sha512', $secret_iv), 0, 16);
+        $iv = substr(hash('sha512', self::$secret_iv), 0, 16);
         
         if ( $action == 'encrypt' ) {
         
-            $output = openssl_encrypt($source, "AES-256-CBC", $key, 0, $iv);
+            $output = openssl_encrypt($source, "AES-256-CBC", $secretkey, 0, $iv);
             $output = base64_encode($output);
         
         } else if( $action == 'decrypt' ) {
         
-            $output = openssl_decrypt(base64_decode($source), "AES-256-CBC", $key, 0, $iv);
+            $output = openssl_decrypt(base64_decode($source), "AES-256-CBC", $secretkey, 0, $iv);
         }
         
         return $output;        
